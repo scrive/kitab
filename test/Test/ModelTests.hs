305071a045b4be3e0e6a7ecdcfa312205cf0d46b
Théophile Choutri de Tarlé <theophile.choutri@scrive.com>
2026-01-28 16:17:57 +0100

    Begin the test suite

--- test/Test/ModelTests.hs
+++ test/Test/ModelTests.hs
@@ -1,1 +1,48 @@
 module Test.ModelTests where
+
+import Data.List.NonEmpty qualified as NE
+import Test.Tasty
+import Validation (validationToEither)
+
+import Core.Model
+import Core.Validation
+import Test.Utils
+
+test :: TestTree
+test =
+  testGroup
+    "Domain model"
+    [ testGroup
+        "Graph consistency validation"
+        [ testThat "Parallel connections are forbidden" testParallelConnectionsDetection
+        , testThat "Self-referential connections are forbidden" testSelfReferentialConnections
+        , testThat "Mismatched connections are forbiddden" testMismatchedConnections
+        ]
+    ]
+
+testParallelConnectionsDetection :: TestEff ()
+testParallelConnectionsDetection = do
+  let graph = build [Service "A" [Connection "B" HTTPS, Connection "B" FunctionCall]]
+  validationError <- assertLeft "Graph is not validated" $ validationToEither (checkGraph graph)
+  assertEqual
+    "Parallel edges"
+    (NE.singleton (Parallel "A" "B" [HTTPS, FunctionCall]))
+    validationError
+
+testSelfReferentialConnections :: TestEff ()
+testSelfReferentialConnections = do
+  let graph = build [Service "A" [Connection "A" HTTPS]]
+  validationError <- assertLeft "Graph is not validated" $ validationToEither (checkGraph graph)
+  assertEqual
+    "Self-Referential edges"
+    (NE.singleton (SelfReferential "A"))
+    validationError
+
+testMismatchedConnections :: TestEff ()
+testMismatchedConnections = do
+  let graph = build [Service "A" [Connection "B" HTTPS], Service "B" [Connection "A" FunctionCall]]
+  validationError <- assertLeft "Graph is not validated" $ validationToEither (checkGraph graph)
+  assertEqual
+    "Mismatched connections"
+    (NE.singleton (Mismatched (("A", "B", HTTPS), ("B", "A", FunctionCall))))
+    validationError
---
+++ test/Test/Utils.hs
@@ -0,0 +1,47 @@
+module Test.Utils
+  ( TestEff
+  , testThat
+  , assertEqual
+  , assertBool
+  , assertFailure
+  , assertJust
+  , assertNothing
+  , assertRight
+  , assertLeft
+  ) where
+
+import Effectful
+import GHC.Stack
+import Test.Tasty (TestTree)
+import Test.Tasty.HUnit qualified as Test
+
+type TestEff a =
+  Eff '[IOE] a
+
+testThat :: String -> TestEff () -> TestTree
+testThat name assertion = Test.testCase name (runEff assertion)
+
+assertEqual :: (Eq a, HasCallStack, Show a) => String -> a -> a -> TestEff ()
+assertEqual message expected actual = liftIO $ Test.assertEqual message expected actual
+
+assertBool :: HasCallStack => String -> Bool -> TestEff ()
+assertBool message assertion = liftIO $ Test.assertBool message assertion
+
+assertFailure :: HasCallStack => String -> TestEff ()
+assertFailure = liftIO . Test.assertFailure
+
+assertJust :: HasCallStack => String -> Maybe a -> TestEff a
+assertJust _ (Just a) = pure a
+assertJust message Nothing = liftIO $ Test.assertFailure message
+
+assertNothing :: HasCallStack => String -> Maybe a -> TestEff ()
+assertNothing message (Just _) = liftIO $ Test.assertFailure message
+assertNothing _ Nothing = pure ()
+
+assertRight :: HasCallStack => String -> Either a b -> TestEff b
+assertRight _ (Right b) = pure b
+assertRight message (Left _a) = liftIO $ Test.assertFailure message
+
+assertLeft :: HasCallStack => String -> Either a b -> TestEff a
+assertLeft description (Right _b) = liftIO $ Test.assertFailure description
+assertLeft _ (Left a) = pure a
