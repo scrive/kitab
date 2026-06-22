984a86485493aaacfc7a7f48bd1f10976623cff3
Théophile Choutri de Tarlé <theophile.choutri@scrive.com>
2026-06-22 16:46:08 +0200

    Re-organise the tests into subgroups

--- test/Main.hs
+++ test/Main.hs
@@ -3,24 +3,40 @@ module Main (main) where
 import Test.Tasty
 
 import Test.CLI.DumpTests qualified as DumpTests
+import Test.Driver.CiliumTests qualified as Driver.CiliumTests
 import Test.InventoryTests qualified as InventoryTests
 import Test.ModelTests qualified as ModelTests
 import Test.ParserTests qualified as ParserTests
-import Test.Render.CiliumTests qualified as CiliumTests
-import Test.Render.GEXFTests qualified as GEXFTests
-import Test.Render.PumlTests qualified as PumlTests
+import Test.Render.CiliumTests qualified as Render.CiliumTests
+import Test.Render.GEXFTests qualified as Render.GEXFTests
+import Test.Render.PumlTests qualified as Render.PumlTests
 
 main :: IO Unit
 main =
-  defaultMain $ testGroup "Kitab Tests" tests
+  defaultMain $
+    testGroup
+      "Kitab Tests"
+      [ testGroup "Model Tests" modelTests
+      , testGroup "Render Tests" renderTests
+      , testGroup "Driver Tests" driverTests
+      ]
 
-tests :: List TestTree
-tests =
+modelTests :: List TestTree
+modelTests =
   [ ModelTests.test
   , DumpTests.test
   , ParserTests.test
-  , PumlTests.test
-  , CiliumTests.test
   , InventoryTests.test
-  , GEXFTests.test
+  ]
+
+renderTests :: List TestTree
+renderTests =
+  [ Render.PumlTests.test
+  , Render.CiliumTests.test
+  , Render.GEXFTests.test
+  ]
+
+driverTests :: List TestTree
+driverTests =
+  [ Driver.CiliumTests.test
   ]
